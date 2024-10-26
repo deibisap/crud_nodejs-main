762e06738b8d9556f5d308bc2f5e279a8d29ba26
Deibis <adayam85@gmail.com>
2024-10-25 19:33:24 -0500

    crud completo

---
+++ controllers/crud.js
@@ -0,0 +1,32 @@
+//Invocamos a la conexion de la DB
+const conexion = require('../database/db');
+//GUARDAR un REGISTRO
+exports.save = (req, res)=>{
+    const nombre= req.body.nombre;
+    const apellido = req.body.apellido;
+    const telefono= req.body.telefono;
+    const correo= req.body.correo;
+    conexion.query('INSERT INTO clientes SET ?', {nombre:nombre, apellido:apellido, telefono:telefono, correo:correo}, (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{
+            //console.log(results);   
+            res.redirect('/');         
+        }
+});
+};
+//ACTUALIZAR un REGISTRO
+exports.update = (req, res)=>{
+    const id = req.body.idcliente;
+    const nombre= req.body.nombre;
+    const apellido = req.body.apellido;
+    const telefono= req.body.telefono;
+    const correo= req.body.correo;
+    conexion.query('UPDATE clientes SET ? WHERE idcliente = ?',[{nombre:nombre, apellido:apellido, telefono:telefono, correo:correo}, id], (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{           
+            res.redirect('/');         
+        }
+});
+};
